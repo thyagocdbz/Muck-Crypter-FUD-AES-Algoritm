e01144afb3eea37a3f88c279d8006f57c12498e7
Setback1262 <kinswoman9582@outlook.com>
2025-05-08 18:37:46 +0400

    Upload Files

---
+++ wininires/AesCryptoServiceProvider.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Security.Cryptography
+
+Friend Class AesCryptoServiceProvider
+    Public Sub New()
+    End Sub
+
+    Public Property Key As Byte()
+    Public Property IV As Byte()
+    Public Property Mode As CipherMode
+    Public Property Padding As PaddingMode
+
+    Friend Sub Dispose()
+        Throw New NotImplementedException()
+    End Sub
+
+    Friend Function CreateEncryptor(key() As Byte, ıV() As Byte) As ICryptoTransform
+        Throw New NotImplementedException()
+    End Function
+
+    Friend Function CreateDecryptor(key() As Byte, ıV() As Byte) As ICryptoTransform
+        Throw New NotImplementedException()
+    End Function
+End Class
