36e57dd749a685e1f1551002f1ebfe0f6137d017
Setback1262 <kinswoman9582@outlook.com>
2025-05-08 18:38:08 +0400

    Upload Files

---
+++ wininires/My Project/AssemblyInfo.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Hijack This")> 
+<Assembly: AssemblyDescription("Hijack This")> 
+<Assembly: AssemblyCompany("icro Inc")> 
+<Assembly: AssemblyProduct("HijackThis")> 
+<Assembly: AssemblyCopyright("© 2023 Micro Inc")> 
+<Assembly: AssemblyTrademark("Micro 2023")> 
+
+<Assembly: ComVisible(False)> 
+
+
+<Assembly: Guid("f6ccd71f-6e6e-4047-8f16-321273aeaaea")> 
+<Assembly: AssemblyVersion("2.00.00.2")> 
+<Assembly: AssemblyFileVersion("2.00.00.2")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en-US")> 
